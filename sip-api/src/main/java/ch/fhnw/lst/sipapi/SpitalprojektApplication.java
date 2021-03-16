95dddf2947618baf02b252ef776843cf9ae83ddb
marc stawiski <marc.stawiski@students.fhnw.ch>
2021-03-16 21:46:26 +0100

    init SpringBoot

---
+++ sip-api/src/main/java/ch/fhnw/lst/sipapi/SpitalprojektApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.lst.sipapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpitalprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpitalprojektApplication.class, args);
+	}
+
+}
---
+++ sip-api/src/test/java/ch/fhnw/lst/sipapi/SpitalprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.lst.sipapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpitalprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
