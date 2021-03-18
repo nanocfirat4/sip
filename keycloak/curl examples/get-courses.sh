599867dc1969dd2b7c72befd90d1ee4894dedc6e
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-03-18 10:48:31 +0100

    create branch stephan

---
+++ keycloak/curl examples/get-courses.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Note: must add header -H "Authorization: Bearer $TOKEN"
+#
+curl http://localhost:8081/api/course/1
---
+++ keycloak/curl examples/post-course.sh
@@ -0,0 +1,2 @@
+curl -H "Authorization: Bearer $TOKEN" http://localhost:8081/api/course \
+     -d '{"name": "Test", "lecturer": "Test"}'
