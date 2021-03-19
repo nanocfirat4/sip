22568dd967d34d240edfbd29921083016d4103a7
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-03-19 19:37:58 +0100

    setup keycloak react

--- sip-React/course-react/src/components/AppNavBar.jsx
+++ sip-React/course-react/src/components/AppNavBar.jsx
@@ -1,15 +1,10 @@
 
-import { useKeycloak } from '@react-keycloak/web'
 import React from 'react'
 import { Navbar, Nav, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-import { courseService } from '../services/CourseService'
+import keycloak from '../keycloak'
 
 function AppNavBar() {
-    const { keycloak } = useKeycloak()
-    console.log(keycloak)
-    courseService.authToken(keycloak.token)
-
     return (
         <Navbar bg='dark' expand='lg' variant='dark'>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
@@ -18,31 +13,22 @@ function AppNavBar() {
                     <LinkContainer to='/'>
                         <Nav.Link>Home</Nav.Link>
                     </LinkContainer>
-                    {keycloak?.authenticated &&
                         <LinkContainer to='/courses'>
                             <Nav.Link>Courses</Nav.Link>
-                        </LinkContainer>}
-                    {keycloak?.authenticated &&
+                        </LinkContainer>
                         <LinkContainer to='/about'>
                             <Nav.Link>Search</Nav.Link>
-                        </LinkContainer>}
+                        </LinkContainer>
                 </Nav>
-                {!keycloak?.authenticated &&
                     <Navbar.Text>
                         <Button
                             variant='dark'
-                            onClick={() => keycloak.login()}
-                        >Login
-                        </Button>
-                    </Navbar.Text>}
-                {keycloak?.authenticated &&
-                    <Navbar.Text>{keycloak?.idTokenParsed?.name}{' '}
-                        <Button
-                            variant='dark'
-                            onClick={() => keycloak.logout()}
+                            onClick={() =>
+                                keycloak.logout()
+                            }
                         >Logout
-</Button>
-                    </Navbar.Text>}
+                        </Button>
+                    </Navbar.Text>
             </Navbar.Collapse>
         </Navbar>
     )
--- sip-React/course-react/src/keycloak.js
+++ sip-React/course-react/src/keycloak.js
@@ -1,8 +1,8 @@
 import Keycloak from 'keycloak-js'
 import { config } from './config'
 const keycloak = new Keycloak({
-url: `${config.KEYCLOAK_BASE_URL}/auth`,
-realm: 'FHNW-LST-MI',
-clientId: "web-app"
+    url: `${config.KEYCLOAK_BASE_URL}/auth`,
+    realm: 'FHNW-LST-MI',
+    clientId: "web-app"
 })
 export default keycloak
