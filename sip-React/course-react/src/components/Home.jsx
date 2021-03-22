419404d27804c25c706c476232b26a6d01dfb5e9
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-03-22 13:14:26 +0100

    Basic Homescreen view

--- sip-React/course-react/src/components/Home.jsx
+++ sip-React/course-react/src/components/Home.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Thumbnail from './Thumbnail'
+import Slider from '@material-ui/core/Slider';
+
+function valuetext(value) {
+    return `${value}px`;
+}
+
+
 function Home () {
+    const handleSliderChange = (event, newValue) => {
+        var items = document.getElementsByClassName("thumbnail")
+
+        for (var i = 0; i < items.length; i++) {
+            items[i].style.width=(newValue+"px");
+            items[i].style.height=(newValue*0.8+"px");
+        }
+    };
+
     return (
         <div className="mt-3">
-            <h1>React - OpenID Connect Example</h1>
-            <p>An example app showing how to secure React with Keycloak.</p>
-            <p><Link to='/courses'>&gt;&gt; Manage Courses</Link></p>
+            <Slider
+                defaultValue={30}
+                getAriaValueText={valuetext}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                step={50}
+                marks
+                min={50}
+                max={400}
+                defaultValue={250}
+
+                onChange={handleSliderChange}
+            />
+            <div class="row">
+                <div id="thumbnail_view">
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                    <Thumbnail />
+                </div>
+                <div id="comments_tags">
+                    Kommentare und Tags
+                </div>
+            </div>
         </div>
     )
 }
---
+++ sip-React/course-react/src/components/Thumbnail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Thumbnail () {
+    return (
+        <div class="thumbnail">
+            test
+            <input id="test" class="thumbnail_checkbox" type="checkbox"></input>
+        </div>
+    )
+}
+export default Thumbnail
