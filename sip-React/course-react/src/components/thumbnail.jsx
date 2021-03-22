a3139e528a2bb9315a1f7ec5fba11723ea375b15
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-03-22 11:06:50 +0100

    initialize new branch

---
+++ sip-React/course-react/src/components/thumbnail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Thumbnail () {
+    return (
+        <div className="thumbnail">
+        </div>
+    )
+}
+export default Home
