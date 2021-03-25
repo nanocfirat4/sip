ed6368e7045994ef29685a3593d18b29471d2598
Firat Turan <firat.turan@students.fhnw.ch>
2021-03-25 16:38:19 +0100

    Image Slide View

--- sip-api/src/main/java/ch/fhnw/lst/sipapi/controller/searchController.java
+++ sip-api/src/main/java/ch/fhnw/lst/sipapi/controller/searchController.java
@@ -3,7 +3,6 @@ package ch.fhnw.lst.sipapi.controller;
 import ch.fhnw.lst.sipapi.model.Image;
 import ch.fhnw.lst.sipapi.model.Search_Favorites;
 import ch.fhnw.lst.sipapi.service.ImageService;
-import ch.fhnw.lst.sipapi.service.SearchFavService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- sip-api/src/main/java/ch/fhnw/lst/sipapi/repository/ImageRepository.java
+++ sip-api/src/main/java/ch/fhnw/lst/sipapi/repository/ImageRepository.java
@@ -1,10 +1,8 @@
 package ch.fhnw.lst.sipapi.repository;
 
 import ch.fhnw.lst.sipapi.model.Image;
-import ch.fhnw.lst.sipapi.model.Search_Favorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 
 public interface ImageRepository extends JpaRepository<Image, Long> {}
