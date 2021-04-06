2ce8ab71fb7476b2db229d651a13db1f8c319bcf
Marc Stawiski <marc.stawiski@students.fhnw.ch>
2021-04-06 16:07:12 +0200

    corrected yml CICD add docker compose up to test and build provisorisch

--- sip-api/src/test/java/ch/fhnw/lst/sipapi/SpitalProjektApplicationTest.java
+++ sip-api/src/test/java/ch/fhnw/lst/sipapi/SpitalProjektApplicationTest.java
@@ -1,6 +1,7 @@
 package ch.fhnw.lst.sipapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
