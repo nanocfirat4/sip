b80fcdf26f422f251f580c8d9fb6441cccaf9479
Marc Stawiski <marc.stawiski@students.fhnw.ch>
2021-04-16 20:17:56 +0200

    add unexpected Exceptionhandler to sip-api

--- sip-api/src/main/java/ch/fhnw/lst/sipapi/exception/RestExceptionHandler.java
+++ sip-api/src/main/java/ch/fhnw/lst/sipapi/exception/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,5 +17,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
       RuntimeException ex, WebRequest request) {
     return handleExceptionInternal(ex, new ch.fhnw.lst.sipapi.exception.ErrorInfo("not found", ex),
         new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ResponseEntity<Object> handleAllUncaughtException(
+          RuntimeException ex,
+          WebRequest request
+  ){
+    return handleExceptionInternal(ex, new ch.fhnw.lst.sipapi.exception.ErrorInfo("Unexpected Internal Server Error", ex),
+            new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
   }
 }
