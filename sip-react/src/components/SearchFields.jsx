73ea86fb7925baa2a07dedeb43bf8ea5b1b1128f
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-04-18 15:54:13 +0200

    Search on enter

--- sip-react/src/components/SearchFields.jsx
+++ sip-react/src/components/SearchFields.jsx
@@ -33,70 +33,75 @@ const SearchFields = () => {
             .then(dispatch({type: "SET_LOADING", payload: false}))
     }
 
+    // Search when enter ist pressed in comments field
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    }
+
     
     // Rendering
     return (
-        <form noValidate autoComplete="off">
-            <Row style={{ minHeight: "70px" }}>
-                <Col md={4}>
-                    <Autocomplete
-                        multiple
-                        id="combo-box-demo"
-                        options={state.allTags}
-                        defaultValue={state.searchTags}
-                        filterSelectedOptions
-                        getOptionLabel={(option) => option.hashtagtxt}
-                        renderInput={(params) =>
-                            <TextField 
-                                {...params}
-                                label="Tags"
-                            />
-                        }
-                        style={{ width: "100%" }}
-                        onChange={(event, values) => dispatch({type: "SET_SEARCH_TAGS", payload: values})}
-                    />
-                </Col>
-                <Col md={4}>
-                    <TextField
-                        id="comment-search"
-                        label="Comments"
-                        type="search"
-                        defaultValue={state.searchComments}
-                        onChange={(event) => dispatch({type: "SET_SEARCH_COMMENTS", payload: event.target.value})}
-                        style={{ width: "100%" }}
-                    />
-                </Col>
-                <Col md={2} xs={5}>
-                    <Button
-                        onClick={handleSearch}
-                        variant="contained"
-                        color="primary"
-                        style={{ margin: "5px" }}
-                        type="submit"
-                    >
-                        Search
-                    </Button>
-                </Col>
-                <Col md={2} xs={7}>
-                    <Typography style={{ textAlign: "center" }}>
-                        Thumbnail size
-                    </Typography>
-                    <Slider
-                        // getAriaValueText={this.valuetext}
-                        aria-labelledby="discrete-slider"
-                        valueLabelDisplay="auto"
-                        step={50}
-                        marks
-                        min={50}
-                        max={300}
-                        defaultValue={150}
-                        id="sizeSlider"
-                        style={{ width: "100%" }}
-                        onChange={handleSliderChange}
-                    />
-                </Col>
-            </Row>
-        </form>
+        <Row style={{ minHeight: "70px" }}>
+            <Col md={4}>
+                <Autocomplete
+                    multiple
+                    id="combo-box-demo"
+                    options={state.allTags}
+                    defaultValue={state.searchTags}
+                    filterSelectedOptions
+                    getOptionLabel={(option) => option.hashtagtxt}
+                    renderInput={(params) =>
+                        <TextField 
+                            {...params}
+                            label="Tags"
+                        />
+                    }
+                    style={{ width: "100%" }}
+                    onChange={(event, values) => dispatch({type: "SET_SEARCH_TAGS", payload: values})}
+                />
+            </Col>
+            <Col md={4}>
+                <TextField
+                    id="comment-search"
+                    label="Comments"
+                    type="search"
+                    defaultValue={state.searchComments}
+                    onChange={(event) => dispatch({type: "SET_SEARCH_COMMENTS", payload: event.target.value})}
+                    style={{ width: "100%" }}
+                    onKeyDown={handleKeyDown}
+                />
+            </Col>
+            <Col md={2} xs={5}>
+                <Button
+                    onClick={handleSearch}
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: "5px" }}
+                >
+                    Search
+                </Button>
+            </Col>
+            <Col md={2} xs={7}>
+                <Typography style={{ textAlign: "center" }}>
+                    Thumbnail size
+                </Typography>
+                <Slider
+                    // getAriaValueText={this.valuetext}
+                    aria-labelledby="discrete-slider"
+                    valueLabelDisplay="auto"
+                    step={50}
+                    marks
+                    min={50}
+                    max={300}
+                    defaultValue={150}
+                    id="sizeSlider"
+                    style={{ width: "100%" }}
+                    onChange={handleSliderChange}
+                />
+            </Col>
+        </Row>
     );
 }
 export default SearchFields
