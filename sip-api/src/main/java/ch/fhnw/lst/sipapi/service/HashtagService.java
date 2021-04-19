4a16099cf07729159475bbb85bf7252936701cb7
Marc Stawiski <marc.stawiski@students.fhnw.ch>
2021-04-19 17:01:04 +0200

    Backend sort Hashtags

--- sip-api/src/main/java/ch/fhnw/lst/sipapi/service/HashtagService.java
+++ sip-api/src/main/java/ch/fhnw/lst/sipapi/service/HashtagService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -16,7 +17,9 @@ public class HashtagService {
     HashtagRepository hashtagRepository;
 
     public List<Hashtag> findAll(){
-        return hashtagRepository.findAll();
+        List<Hashtag> allHashtags = hashtagRepository.findAll();
+        allHashtags.sort(Comparator.comparingInt(Hashtag::getHashtagCount));
+        return allHashtags;
     }
     public Hashtag findById(Long id){
         return hashtagRepository.findById(id).orElseThrow(() ->
