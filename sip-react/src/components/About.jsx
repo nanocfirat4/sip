22f18f18fe61eb82e11b49699ecb7ae750f76626
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-04-20 21:49:37 +0200

    Tag Statistics

--- sip-react/src/components/About.jsx
+++ sip-react/src/components/About.jsx
@@ -1,5 +1,75 @@
-import React from 'react'
-function About() {
-    return <div className='mt-3'>THIS IS THE ABOUT PAGE</div>
+import { MenuItem, Paper, TextField } from '@material-ui/core'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../Store'
+
+import {
+    Chart,
+    BarSeries,
+    Title,
+    ArgumentAxis,
+    ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+import { Animation } from '@devexpress/dx-react-chart';
+import { TagService } from '../services/TagService';
+import keycloak from '../keycloak';
+
+const About = () => {
+    const [state, dispatch] = useContext(Context)
+
+    const [fromIndex, setFromIndex] = useState(0)
+    const [toIndex, setToIndex] = useState(15)
+    console.log(state.allTags)
+
+    useEffect(() => {
+        TagService.authToken(keycloak.token)
+        TagService.findAll().then(res => dispatch({ type: "SET_ALL_TAGS", payload: res.data }))
+    }, [])
+
+
+    return <div className='mt-3'>
+        <TextField
+            id="from_index"
+            select
+            value={fromIndex}
+            label="From Rank"
+            onChange={(event) => setFromIndex(event.target.value)}
+            style={{ margin: "20px", width: "100px" }}
+        >
+            {Array.from({ length: toIndex }, (v, i) => i).map((option) => (
+                <MenuItem key={option} value={option}>
+                    {option}
+                </MenuItem>
+            ))}
+        </TextField>
+
+        <TextField
+            id="to_index"
+            select
+            value={toIndex}
+            label="To Rank"
+            onChange={(event) => setToIndex(event.target.value)}
+            style={{ margin: "20px", width: "100px" }}
+        >
+            {Array.from({ length: state.allTags.length - fromIndex }, (v, i) => i + fromIndex + 1).map((option) => (
+                <MenuItem key={option} value={option}>
+                    {option}
+                </MenuItem>
+            ))}
+        </TextField>
+        
+        <Paper>
+            <Chart data={state.allTags.slice(fromIndex, toIndex)}>
+                <ArgumentAxis />
+                <ValueAxis may={7} />
+
+                <BarSeries
+                    valueField="hashtagCount"
+                    argumentField="hashtagtxt"
+                />
+                <Title text="Tag usage" />
+                <Animation />
+            </Chart>
+        </Paper>
+    </div>
 }
 export default About
