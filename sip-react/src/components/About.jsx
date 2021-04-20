774206ad55d994bb3936ac0524b73dd704299d8f
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-04-20 21:58:18 +0200

    Correct issue with showing ranks ins statistic

--- sip-react/src/components/About.jsx
+++ sip-react/src/components/About.jsx
@@ -16,8 +16,8 @@ import keycloak from '../keycloak';
 const About = () => {
     const [state, dispatch] = useContext(Context)
 
-    const [fromIndex, setFromIndex] = useState(0)
-    const [toIndex, setToIndex] = useState(15)
+    const [fromIndex, setFromIndex] = useState(1)
+    const [toIndex, setToIndex] = useState(state.allTags.length > 20 ? 20 : state.allTags.length)
     console.log(state.allTags)
 
     useEffect(() => {
@@ -35,7 +35,7 @@ const About = () => {
             onChange={(event) => setFromIndex(event.target.value)}
             style={{ margin: "20px", width: "100px" }}
         >
-            {Array.from({ length: toIndex }, (v, i) => i).map((option) => (
+            {Array.from({ length: toIndex }, (v, i) => i + 1).map((option) => (
                 <MenuItem key={option} value={option}>
                     {option}
                 </MenuItem>
@@ -50,7 +50,7 @@ const About = () => {
             onChange={(event) => setToIndex(event.target.value)}
             style={{ margin: "20px", width: "100px" }}
         >
-            {Array.from({ length: state.allTags.length - fromIndex }, (v, i) => i + fromIndex + 1).map((option) => (
+            {Array.from({ length: state.allTags.length - fromIndex }, (v, i) => i + fromIndex + 2).map((option) => (
                 <MenuItem key={option} value={option}>
                     {option}
                 </MenuItem>
