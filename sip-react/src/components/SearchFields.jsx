396231fad5d079f58bd60b7964487b6c42e88f57
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-04-20 11:18:25 +0200

    show hashtag count

--- sip-react/src/components/SearchFields.jsx
+++ sip-react/src/components/SearchFields.jsx
@@ -61,7 +61,7 @@ const SearchFields = () => {
                     options={state.allTags}
                     defaultValue={state.searchTags}
                     filterSelectedOptions
-                    getOptionLabel={(option) => "(" + option.hashtagCount + ")" + option.hashtagtxt}
+                    getOptionLabel={(option) => "(" + option.hashtagCount + ") \t" + option.hashtagtxt}
                     renderInput={(params) =>
                         <TextField
                             {...params}
