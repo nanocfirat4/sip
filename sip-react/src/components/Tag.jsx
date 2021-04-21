6fbb97ea776d33f5f31b74d4718d561280899776
Stephan Häberle <stephan.haeberle@students.fhnw.ch>
2021-04-21 16:39:39 +0200

    Tags in Viewmode

--- sip-react/src/components/Tag.jsx
+++ sip-react/src/components/Tag.jsx
@@ -4,19 +4,69 @@ import keycloak from '../keycloak'
 import { TagService } from '../services/TagService'
 // Material-UI
 import Chip from '@material-ui/core/Chip';
+import { IconButton, Paper, Tooltip } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
 class Tag extends Component {
-    handleDeleteTag(tag) {
+    handleDeleteCommonTag() {
         var i = 1;
 
         TagService.authToken(keycloak.token);
         this.props.selectedImages.map(image => {
-            TagService.remove(image.id, tag).then(() => {
+            TagService.remove(image.id, this.props.tag).then(() => {
                 i === this.props.selectedImages.length ? this.props.updateSelected() : i++;
             })
         })
+    }
 
+    handleDeleteTag() {
+        var i = 1;
+
+        TagService.authToken(keycloak.token);
+        TagService.remove(this.props.currentImage.id, this.props.tag).then(() => {
+            i === this.props.selectedImages.length ? this.props.updateSelected() : i++;
+        });
+    }
+
+
+    getDeleteCurrentButton() {
+        return (
+            <React.Fragment>
+                <Tooltip title={this.props.viewMode ? "Delete for current" : "Delete for selected"} style={{paddingLeft:"0"}}>
+                    <IconButton aria-label="delete">
+                        <DeleteIcon
+                            fontSize="medium"
+                            onClick={this.props.viewMode ? () => this.handleDeleteTag() : () => this.handleDeleteCommonTag()}
+                        />
+                    </IconButton>
+                </Tooltip>
+            </React.Fragment>
+        )
+    }
+
+    getDeleteCommonButtons() {
+        return (
+            <React.Fragment style={{margin: "0", padding:"0"}}>
+                <Tooltip title="Delete for current" style={{paddingLeft: "0", paddingRight: "0"}}>
+                    <IconButton aria-label="delete">
+                        <DeleteIcon
+                            fontSize="medium"
+                            onClick={() => this.handleDeleteTag()}
+                        />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete for all">
+                    <IconButton aria-label="delete">
+                        <DeleteForeverIcon
+                            fontSize="medium"
+                            onClick={this.props.viewMode ? () => this.handleDeleteTag() : () => this.handleDeleteCommonTag()}
+                        />
+                    </IconButton>
+                </Tooltip>
+            </React.Fragment>
+        )
     }
 
     // Rendering
@@ -26,7 +76,14 @@ class Tag extends Component {
             <Chip
                 label={this.props.tag.hashtagtxt}
                 size="medium"
-                onDelete={() => this.handleDeleteTag(this.props.tag)}
+                deleteIcon={
+                    this.props.viewMode ?
+                        this.props.common ?
+                            this.getDeleteCommonButtons()
+                            : this.getDeleteCurrentButton()
+                        : this.getDeleteCurrentButton()
+                }
+                onDelete={() => null}
                 style={{
                     margin: "5px",
                     fontSize: "11pt"
